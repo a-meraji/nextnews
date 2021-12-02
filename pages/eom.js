c2d6c562ef0da8f98e5223b52464f2897e0e6edf
a-meraji <merajiamin1997@gmail.com>
2021-12-02 23:19:01 +0330

    init commit

---
+++ pages/eom.js
@@ -0,0 +1,30 @@
+const EOM = ({ employee }) => {
+  return (
+    <div className="page-container">
+      <div>
+        <h1>employee of the month</h1>
+        <div>
+            <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={employee.image}/>
+            <p>{employee.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (pageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth"
+  );
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee,
+    },
+  };
+};
+
+export default EOM;
